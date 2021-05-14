ca0a7577a52ae0e0481b0525cd2afd78517e3d7c
AlesonTalis <alesontor@gmail.com>
2021-05-13 23:46:29 -0300

    Easy Unity's Vector2 and Vector3
    cria o vector2 ou vector3 com apenas um pequeno comando: F.V2 ou F.V3

---
+++ Unity_VectorCreator.cs
@@ -0,0 +1,23 @@
+/**
+ * A fast and easy mode to create a Vector in Unity
+*/
+
+public static partial class F
+{
+  /// <summary>
+  /// Create a Vector2
+  /// </summary>
+  /// <param name="x">the x value</param>
+  /// <param name="y">the y value</param>
+  /// <returns>a new Vector2</returns>
+  public static Vector2 V2(float x, float y) => new Vector2() { x = x, y = y };
+
+  /// <summary>
+  /// Create a Vector2
+  /// </summary>
+  /// <param name="x">the x value</param>
+  /// <param name="y">the y value</param>
+  /// <param name="z">the z value</param>
+  /// <returns>a new Vector3</returns>
+  public static Vector3 V3(float x, float y, float z) => new Vector3() { x = x, y = y, z = z };
+}
